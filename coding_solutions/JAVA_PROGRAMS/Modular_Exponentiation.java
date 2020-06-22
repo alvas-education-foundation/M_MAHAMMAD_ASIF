908f3530fbcb5287e4c68c900ad2374d86dac50b
mdasif9900 <45789445+mdasif9900@users.noreply.github.com>
2020-06-22 21:34:36 +0530

    Update Modular_Exponentiation.java

--- coding_solutions/JAVA_PROGRAMS/Modular_Exponentiation.java
+++ coding_solutions/JAVA_PROGRAMS/Modular_Exponentiation.java
@@ -3,6 +3,7 @@ import java.lang.Math;
 class Modular_Exponentiation{
 	public static void main(String args[]){
 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the values of x,y and p seperated by space");
 	double x=sc.nextInt();
 	double y=sc.nextInt();
 	int p=sc.nextInt();
